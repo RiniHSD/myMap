f24e3dc690959c5c488267d60a3ace280ed86e6c
RiniHSD <rinihusadiyah@gmail.com>
2023-09-17 11:34:41 +0700

    menampilkan peta sederhana menggunakan ArcGIS API sdk for JavaScript

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, numberAttribute, OnInit } from '@angular/core';
+import Map from '@arcgis/core/Map';
+import MapView from '@arcgis/core/views/MapView';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   constructor() {}
+
+  private latitude: number | any;
+  private longitude: number | any;
+
+  public async ngOnInit() {
+    // this.longitude = 101.45211273792678;
+    // this.latitude = 0.7431636232709788;
+    //throw new Error("Method not implemented.");
+
+    const position = await Geolocation.getCurrentPosition(); //untuk menampilkan peta sesuai lokasi user
+    this.latitude = position.coords.latitude;
+    this.longitude = position.coords.longitude;
+
+    const map = new Map({
+      basemap: "topo-vector" //Reference to the base of the map
+    });
+
+    const view = new MapView({
+      container: "container", 
+      map: map, 
+      zoom: 16,
+      center: [this.longitude, this.latitude]
+      
+    });
+
+    
+  }
 
 }
